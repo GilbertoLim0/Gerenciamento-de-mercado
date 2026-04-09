f375a9e4a57b74069dcb2a1ac9e7a843c6ee260c
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-04-09 10:03:36 -0300

    saving

--- backend/App.js
+++ backend/App.js
@@ -1,4 +1,50 @@
-const prompt = require("prompt-sync")
+import promptSync from 'prompt-sync';
+const prompt = promptSync({sigint: true})()
+function main() {
+    console.log(
+   "\n======================================"
+ + "\n📦 SISTEMA DE ESTOQUE SUPERMERCADO "
+ + "\n====================================== "
+ + "\n1. Adicionar Novo Produto "
+ + "\n2. Listar Todos os Produtos "
+ + "\n3. Buscar Produto por Nome "
+ + "\n4. Editar Informações de um Produto "
+ + "\n5. Registrar Entrada/Saída de Estoque "
+ + "\n6. Remover Produto "
+ + "\n0. Sair do Sistema "
+ + "\n====================================== "
+ + "\nEscolha uma opção: "
+)
+    switch(prompt()) {
+        case 1: {
 
+        }
+        case 2: {
 
-variavel = prompt()
+        }
+        case 3: {
+
+        }
+        case 4: {
+
+        }
+        case 5: {
+
+        }
+        case 6: {
+
+        }
+        case 0: {
+
+        }
+        
+    }
+}
+
+function addNewProduct() {}
+function readAllproducts() {}
+function readProductByname() {}
+function updateProductInfo() {}
+function addNewProduct() {}
+function addNewProduct() {}
+function addNewProduct() {}
