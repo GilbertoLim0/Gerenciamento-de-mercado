c40db2f870e8ce5e3a959dc75798ef9a5475dfa6
GILBERTO CLEBE DE SOUZA LIMA <gilberto_c_lima@estudante.sesisenai.org.br>
2026-04-09 10:48:51 -0300

    readAllProducts

--- backend/Functions/Read.js
+++ backend/Functions/Read.js
@@ -1,3 +1,16 @@
 export function findProductByName(database, name) {
     return database.find(product => product.name = name)
 }
+
+export function readAllProduct(database) {
+    return database.array.forEach(product => {
+        console.log(
+            "ID: " + product.id +
+            "Nome: " + product.nome +
+            "Marca: " + product.marca +
+            "Preço: " + product.preco +
+            "Categoria: " + product.categoria +
+            "Quantidade: " + product.quantidade 
+        )
+    });
+}
